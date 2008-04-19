2a3b25311e1e42f1091cd2e42d83d14c46aa1c58
Myron Marston <myron.marston@gmail.com>
2008-04-19 09:11:53 +0000

    Fixed a typo.

--- FractalComposer/src/com/myronmarston/music/transformers/Transformer.java
+++ FractalComposer/src/com/myronmarston/music/transformers/Transformer.java
@@ -4,7 +4,7 @@ import com.myronmarston.music.NoteList;
 
 /**
  * Implementations of this interface are used to take a NoteList and transform 
- * it into a new NoteList, using some kind of musical operation.  hese will be 
+ * it into a new NoteList, using some kind of musical operation.  These will be 
  * combined in various ways to generate the fractal music.
  * 
  * @author Myron
